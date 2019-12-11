0b48d6d6658ac7f820e4b6c94463b77324a915d2
ismaeel96 <jamil.ismaeel@gmail.com>
2019-12-10 21:53:34 -0500

    Update config.py
    moved database credentials to config

--- config.py
+++ config.py
@@ -2,3 +2,8 @@ TOKEN = "TOKENHERE"
 REDDIT_ID = 'scriptidhere'
 REDDIT_SECRET = 'secrethere'
 USER_AGENT = 'useragenthere(google to find it)'
+
+DB_IP = 'DatabaseHostAddress'
+DB_USER = 'DatabaseUserName'
+DB_PASSWORD = 'DatabasePassword'
+DB_NAME = 'DatabaseName'
